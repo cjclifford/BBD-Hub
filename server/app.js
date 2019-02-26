fe98d15bf35584ee6ce4b253c41523a2c5162b54
Khutjo Maputla <khutjom@bbd.co.za>
2019-02-26 14:49:53 +0200

    add

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
-const http = require('http');
+const express = require('express');
 const hostname = '127.0.0.1';
 const port = '4000';
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello');
-});
+const app = express();
 
-server.listen(port, hostname, () => {
+app.get('/', function (req, res) {
+    res.send('hello world')
+  })
+
+app.listen(port, () => {
     console.log(`Server running at https://${hostname}:${port}`);
-});
+  });
