7d892ef0afa5df66ba62ae6d95d26aa9c6974540
Cuan Clifford <cuanc@bbd.co.za>
2019-02-26 16:28:14 +0200

    mssql

--- server/app.js
+++ server/app.js
@@ -1,19 +1,24 @@
 const express = require('express');
+const sql = require('mssql');
+
 const hostname = '127.0.0.1';
 const port = '4000';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    sql.connect(config, (error) => {
+        if (error) console.log(error);
+    });
     res.status(200).send('hello world');
 });
 
-app.get('/request', (req, res) => {
+app.get('/request', async (req, res) => {
     // query database for requests
     res.status(200).send(`get request success`);
 });
 
-app.delete('/request', (req, res) => {
+app.delete('/request', async (req, res) => {
     // delete specified request from database
     res.status(200).send(`DELETE request success`);
 });
