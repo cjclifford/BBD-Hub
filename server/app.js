e9881c6513846778e702d9823d7b3fbd0d872f47
Cuan Clifford <cuanc@bbd.co.za>
2019-02-26 15:05:17 +0200

    get, delete for requests

--- server/app.js
+++ server/app.js
@@ -6,8 +6,18 @@ const app = express();
 
 app.get('/', function (req, res) {
     res.send('hello world')
-  })
+});
+
+app.get('/request', (res, req) => {
+    // query database for requests
+    res.statusCode(200).send(`get request success`);
+});
+
+app.delete('/request', (res, req) => {
+    // delete specified request from database
+    res.statusCode(200).send(`DELETE request success`);
+});
 
 app.listen(port, () => {
     console.log(`Server running at https://${hostname}:${port}`);
-  });
+});
