80b6bb3dd3cff89e2f732b09cd8c2fec12d76889
Khutjo Maputla <khutjom@bbd.co.za>
2019-02-27 14:42:37 +0200

    hold

--- server/route/route.js
+++ server/route/route.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 
 router.post('/login/user', (req, res) => {
-    res.send("hello");
+    // res.send("hello");
+    res.redirect('http://localhost:4200/homepage');
     console.log(req);
 });
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { MapComponent } from './map/map.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { RequestViewComponent } from './request-view/request-view.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/map', pathMatch: 'full'},
   {path: 'map', component: MapComponent},
   {path: 'homepage', component: HomepageComponent},
-  {path: 'request-view', component: RequestViewComponent}
+  {path: 'request-view', component: RequestViewComponent},
+  {path: 'login/User', component: SignInComponent},
 ];
 
 @NgModule({
