3a60c3d7e82d5e89bd2bdfe968f0a886707658d2
Nkosinathi Ngwenya <nkosinathing@bbd.co.za>
2019-02-27 09:29:15 +0200

    Request_Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { RequestFormComponent } from './request-form/request-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    RequestFormComponent
   ],
   imports: [
     BrowserModule,
