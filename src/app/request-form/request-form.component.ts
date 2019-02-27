a0bc96efb30ccfc2282ba001098dd5bacde94b15
Nkosinathi Ngwenya <nkosinathing@bbd.co.za>
2019-02-27 15:44:20 +0200

    form

--- src/app/request-form/request-form.component.ts
+++ src/app/request-form/request-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-request-form',
@@ -11,5 +11,43 @@ export class RequestFormComponent implements OnInit {
 
   ngOnInit() {
   }
+
+}*/
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-request-form',
+  templateUrl: './request-form.component.html',
+  styleUrls: ['./request-form.component.css']
+})
+export class RequestFormComponent implements OnInit {
+
+    @ViewChild("video")
+    public video: ElementRef;
+
+    @ViewChild("canvas")
+    public canvas: ElementRef;
+
+    public captures: Array<any>;
+
+    public constructor() {
+        this.captures = [];
+    }
+
+    public ngOnInit() { }
+
+    public ngAfterViewInit() {
+        if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+                this.video.nativeElement.src = window.URL.createObjectURL(stream);
+                this.video.nativeElement.play();
+            });
+        }
+    }
+
+    public capture() {
+        var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
+        this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
+    }
 
 }
