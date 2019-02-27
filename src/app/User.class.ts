e448abe7880f3143899319c4631b1e2efed3da3b
Khutjo Maputla <khutjom@bbd.co.za>
2019-02-27 13:47:50 +0200

    khutjo

---
+++ src/app/User.class.ts
@@ -0,0 +1,13 @@
+
+export class User{
+    UserName: String;
+    PassWord: String;
+};
+
+export class signup{
+    FirstName: String;
+    LastName: String;
+    Email: String;
+    UserName: String;
+    PassWord: String;
+};
