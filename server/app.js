7aff0ce40cd3c933788193a04b59cb9d452c1c66
Cuan Clifford <cuanc@bbd.co.za>
2019-02-27 08:08:51 +0200

    db populating script, functions do stuff with db yay

--- server/app.js
+++ server/app.js
@@ -6,29 +6,53 @@ const port = '4000';
 
 const app = express();
 
-app.get('/', async (req, res) => {
-    const config = {
-        user: 'sa',
-        password: 'Password_',
-        server: 'localhost',
-        database: 'BBD_Hub'
-    }
+const config = {
+    user: 'sa',
+    password: 'Password_',
+    server: 'localhost',
+    database: 'BBD_Hub'
+}
+try {
     sql.connect(config, (error) => {
         if (error) console.log(error);
         else console.log('success');
     });
+} catch (error) {
+    throw error;
+}
+
+app.get('/', async (req, res) => {
     res.status(200).send('hello world');
 });
 
 app.get('/request', async (req, res) => {
-    // query database for requests
-    res.status(200).send(`get request success`);
+    request = new sql.Request();
+    request.query('SELECT * FROM Requests', (error, recordSet) => {
+        if (error) throw error;
+        res.status(200).send(recordSet);
+    });
 });
 
 app.delete('/request', async (req, res) => {
-    // delete specified request from database
+    request = sql.Request();
+    request.query(`DELETE FROM Requests WHERE RequestID = ${req.body.id}`);
     res.status(200).send(`DELETE request success`);
 });
+
+app.post('/request', async (req, res) => {
+    request = new sql.Request();
+    request.query(`INSERT INTO Requests(RoomID, FlagID, Status, Description)
+    VALUES (
+        ${req.body.roomId},
+        ${req.body.flagId},
+        0,
+        ${req.body.description}
+    )`,
+    (error, recordSet) => {
+        if (error) throw error;
+        res.status(200).send(recordSet);
+    });
+})
 
 app.listen(port, () => {
     console.log(`Server running at https://${hostname}:${port}`);
