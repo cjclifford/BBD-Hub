3755df6e77e8c8a5265750ad6f23f0f71b879aec
Nkosinathi Ngwenya <nkosinathing@bbd.co.za>
2019-02-28 11:25:07 +0200

    will style in a bit

--- src/app/request-form/request-form.component.ts
+++ src/app/request-form/request-form.component.ts
@@ -1,12 +1,31 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Subject} from 'rxjs/Subject';
+import {Observable} from 'rxjs/Observable';
+import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
 
 @Component({
   selector: 'app-request-form',
   templateUrl: './request-form.component.html',
   styleUrls: ['./request-form.component.css']
 })
-export class RequestFormComponent implements OnInit {
+export class RequestFormComponent {
 
-    public ngOnInit() { }
+  public seconds:number ;
+   private trigger: Subject<void> = new Subject<void>();
 
+  // latest snapshot
+  public webcamImage: WebcamImage = null;
+
+  public triggerSnapshot(): void {
+    this.trigger.next(); 
+  }
+
+  public handleImage(webcamImage: WebcamImage): void {
+    console.info("received webcam image", webcamImage);
+    this.webcamImage = webcamImage;
+  }
+
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
 }
